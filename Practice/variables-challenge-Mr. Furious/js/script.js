6ff42bf6f0e2eccf104884ade9b60a340e91a8e8
Emma Rose <emmaroseforget@gmail.com>
2026-09-25 11:42:08 -0400

    added stuff

--- Practice/variables-challenge-Mr. Furious/js/script.js
+++ Practice/variables-challenge-Mr. Furious/js/script.js
@@ -1,24 +1,38 @@
 /**
- * Title of Project
- * Author Name
+ * Mr. Furious is furious
+ * Emma Rose Forget
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Who made Mr. Furious furious?
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+//  Mr. Furious
+let mrFurious = {
+    // Position and size
+    x: 200,
+    y: 200,
+    size: 100,
+    // Colour
+    fill: {
+        r: 255,
+        g: 225,
+        b: 225
+    }
+};
 
+// creating the canvas
+function setup() {
+    createCanvas(400, 400);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    background(160, 180, 200);
 
+    // Draw Mr. Furious as a coloured circle
+    push();
+    noStroke();
+    fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+    ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+    pop();
 }
