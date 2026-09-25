dc1dbcdd88b5e1f215f77fa4a83a2450dcad4ad7
Emma Rose <emmaroseforget@gmail.com>
2026-09-25 08:43:14 -0400

    added code, changed title

--- Practice/movement-variables/js/script.js
+++ Practice/movement-variables/js/script.js
@@ -8,17 +8,24 @@
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+let bird = {
+    x: 120,
+    y: 480,
+    size: 50
+}
 
+function setup() {
+    createCanvas(640, 480);
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    background(0);
 
+    // Move the bird
+    bird.x = bird.x + 1;
+    bird.y = bird.y - 2;
+
+    // Draw the bird
+    ellipse(bird.x, bird.y, bird.size);
 }
