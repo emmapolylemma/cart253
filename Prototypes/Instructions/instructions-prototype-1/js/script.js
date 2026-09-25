0f30cf3770cfb01ec759e9b7d827392f3fc7e4f3
Emma Rose <emmaroseforget@gmail.com>
2026-09-24 20:32:58 -0400

    Update script.js

--- Prototypes/Instructions/instructions-prototype-1/js/script.js
+++ Prototypes/Instructions/instructions-prototype-1/js/script.js
@@ -9,12 +9,15 @@
 
 // Adding the canvas and adding a circle
 function setup() {
-    createCanvas(350, 350);
+    createCanvas(100, 100, WEBGL);
 
-    background(blue);
+    background(200);
 
+    circle(0, 0, 25);
+
+    describe('A white circle with black outline in the middle of a gray canvas.');
 }
 
 function draw() {
-    circle(50, 50, 25);
+
 }
