18faa4e10bcd87c96c8942a8e286fdbe02ec0049
Emma Rose <emmaroseforget@gmail.com>
2026-09-24 20:13:11 -0400

    changes

--- creating-variables/README.md
+++ creating-variables/README.md
@@ -1,6 +1,6 @@
-# TITLE OF PROJECT
+# Creating Variables
 
-AUTHOR NAME
+Emma Rose Forget
 
 [View this project online](URL_FOR_THE_RUNNING_PROJECT)
 
--- creating-variables/js/script.js
+++ creating-variables/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Creating Variables
+ * Emma Rose Forget
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -8,17 +8,30 @@
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+// Our cheese colour broken into RGB
+let cheeseRed = 255;
+let cheeseGreen = 255;
+let cheeseBlue = 255;
 
+// Our cheese hole
+let holeShade = 0; // Greyscale value for the hole
+let holeX = 140; // x-coordinate of the hole
+let holeY = 175; // y-coordinate of the hole
+let holeSize = 180; // Diameter of the hole
+
+function setup() {
+    // Create the canvas
+    createCanvas(480, 480);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    // Cheese colour (yellow)
+    background(cheeseRed, cheeseGreen, cheeseBlue);
 
+    // Draw a hole in the upper left
+    push();
+    noStroke();
+    fill(holeShade);
+    ellipse(holeX, holeY, holeSize);
+    pop()
 }
