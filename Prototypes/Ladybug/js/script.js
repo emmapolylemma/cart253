900efa210c51ddd441a433541020e88fba2ec64f
Emma Rose <emmaroseforget@gmail.com>
2026-09-25 11:00:25 -0400

    adding stuff to the lady bug

--- Prototypes/Ladybug/js/script.js
+++ Prototypes/Ladybug/js/script.js
@@ -14,11 +14,35 @@ function setup() {
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+// Making the ladybug.
 function draw() {
-    background(200);
+    background(0, 255, 255);
 
-    ellipse(0, 0, 80, 40);
+    // body of the ladybug
+    push();
+    noStroke(8);
+    fill(244, 194, 194);
+    ellipse(250, 250, 150, 200);
+    pop();
+
+    // wonky head of the ladybug
+    push();
+    noStroke(0);
+    fill(0, 0, 0);
+    ellipse(250, 150, 100, 50);
+    pop();
+
+    // spots on the ladybug
+    push();
+    noStroke(0);
+    fill(255, 255, 255);
+    ellipse(290, 250, 50, 50);
+    pop();
+
+    push();
+    noStroke(0);
+    fill(255, 255, 255);
+    ellipse(200, 250, 60, 90);
+    pop();
+
 }
