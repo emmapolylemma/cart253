9e0fbbe24d64ea1b3f74eddd35a78df830bc627c
Emma Rose <emmaroseforget@gmail.com>
2026-09-25 09:02:24 -0400

    first try it works youpie

--- Practice/ufo-on-a-dark-night/js/script.js
+++ Practice/ufo-on-a-dark-night/js/script.js
@@ -7,6 +7,7 @@
 
 "use strict";
 
+
 // Our UFO
 let ufo = {
     // Position
@@ -22,19 +23,35 @@ let ufo = {
 // Shade to fill the sky (background)
 let skyShade = 0;
 
-/**
- * Creates the canvas
-*/
+// creating canvas
 function setup() {
     createCanvas(400, 400);
 }
 
-/**
- * Displays a UFO
-*/
+// display of UFO flying
 function draw() {
+    // Sky shade goes up
+    // The sky gradually turns bright white!
+    skyShade = skyShade + 1;
     // Display the sky
     background(skyShade);
+
+    // The UFO goes up
+    // We have lift-off!
+    ufo.y = ufo.y - 2;
+
+    // The UFO goes right
+    // Course adjustment for Beetlegeuse
+    ufo.x = ufo.x + 0.5;
+
+    // UFO gradually becomes darker
+    // I mean, activating quantum thrust transition...
+    ufo.fill = ufo.fill * 0.999;
+
+    // UFO's dimensions become smaller over time
+    // ... as it flies away into the distance! ET... gone home...
+    ufo.width = ufo.width / 1.005;
+    ufo.height = ufo.height / 1.005;
 
     // Draw the UFO based on its properties
     push();
