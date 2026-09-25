49e1b4d07c7efd8d6c2b97ec39ca2686742f5535
Emma Rose <emmaroseforget@gmail.com>
2026-09-25 08:46:12 -0400

    title and code

--- Practice/movement-variables/js/script.js
+++ Practice/movement-variables/js/script.js
@@ -1,17 +1,27 @@
 /**
- * Title of Project
- * Author Name
+ * Bird
+ * Emma Rose Forget
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
+ * Its a bird, its a plane! No, no its sill a bird. 
 
 "use strict";
 
 let bird = {
     x: 120,
     y: 480,
-    size: 50
+    size: 50,
+    // NEW! We'll set the velocity to start at 0, the bird is not moving
+    velocity: {
+        x: 0,
+        y: 0
+    },
+    // NEW! We now have acceleration properties, these will be ADDED to the
+    // velocity every frame
+    acceleration: {
+        x: 0.025,
+        // The y acceleration is negative so the bird will go UP
+        y: -0.05
+    }
 }
 
 function setup() {
@@ -21,11 +31,18 @@ function setup() {
 
 function draw() {
     background(0);
-
-    // Move the bird
-    bird.x = bird.x + 1;
-    bird.y = bird.y - 2;
-
+    
+    // NEW Change the bird's velocity by adding its acceleration to its
+    // velocity
+    // You can see it's the same idea as with changing the position using
+    // the velocity. We change the velocity using the acceleration.
+    bird.velocity.x = bird.velocity.x + bird.acceleration.x;
+    bird.velocity.y = bird.velocity.y + bird.acceleration.y;
+    
+    // Move the bird by adding its velocity in x and y
+    bird.x = bird.x + bird.velocity.x;
+    bird.y = bird.y + bird.velocity.y;
+    
     // Draw the bird
     ellipse(bird.x, bird.y, bird.size);
 }
