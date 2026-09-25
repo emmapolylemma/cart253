d2a55be4428430823a99ea954402b725df8ad73d
Emma Rose <emmaroseforget@gmail.com>
2026-09-25 08:51:48 -0400

    try 1

--- Practice/movement-variables/js/script.js
+++ Practice/movement-variables/js/script.js
@@ -8,17 +8,34 @@
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+let bird = {
+    x: 120,
+    y: 480,
+    size: 50,
+    // NEW! We've added a velocity property to track the bird's velocity
+    velocity: {
+        // It has x and y properties so we can move the bird in both directions
+        x: 1,
+        // The y is negative because we want the bird to go UP
+        y: -2
+    }
+}
+
+// Creating canva
 function setup() {
-
+    createCanvas(640, 480);
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+//
 function draw() {
+    background(0);
 
-}
+    // Move the bird by adding its velocity in x and y
+    // Note that we always ADD velocity, so we use positive and negative
+    // velocities to control direction
+    bird.x = bird.x + bird.velocity.x;
+    bird.y = bird.y + bird.velocity.y;
+
+    // Draw the bird
+    ellipse(bird.x, bird.y, bird.size);
